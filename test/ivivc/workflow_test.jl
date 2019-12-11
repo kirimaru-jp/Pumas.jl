4217dbd23ce2bc83f9f942e179c9c1d6a7d109d3
Shubham Maddhashiya <shubhamsipah00@gmail.com>
2019-12-11 20:44:34 +0530

    added test for weibull model

--- test/ivivc/workflow_test.jl
+++ test/ivivc/workflow_test.jl
@@ -25,3 +25,18 @@ conc = vivo_data[1]["medium"].conc
 cmax_pe, auc_pe = @test_nowarn percentage_prediction_error(time, conc, sol.t, sol.u)
 @test cmax_pe ≈ 0.6097238263353497
 @test auc_pe  ≈ 0.6511465890043491
+
+# other Fdiss models
+
+model = @test_nowarn IVIVCModel(vitro_data, uir_data, vivo_data, vitro_model=:weibull,
+                                                    time_scale = true,
+                                                    time_shift = true,
+                                                    p = p, lb = lb, ub = ub,
+                                                    uir_frac=1.0, deconvo_method=:wn, ivivc_model=ivivc_model);
+
+sol = @test_nowarn predict_vivo(model, "medium")
+time = vivo_data[1]["medium"].time
+conc = vivo_data[1]["medium"].conc
+cmax_pe, auc_pe = @test_nowarn percentage_prediction_error(time, conc, sol.t, sol.u)
+@test cmax_pe  ≈ 3.4637819216318197
+@test auc_pe  ≈ 5.213585111751991
