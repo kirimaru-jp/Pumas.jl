c7db073b95f825fcb42b9ba03ea3d4a8170ffaa4
Vaibhavdixit02 <vaibhavyashdixit@gmail.com>
2019-12-19 01:44:25 +0530

    Add stratification

--- src/estimation/vpc.jl
+++ src/estimation/vpc.jl
@@ -4,29 +4,56 @@ struct VPC
   simulated
   probabilities
   confidence_level
+  stratify_cvs
+  covariate_values
 end
 
 function Base.show(io::IO, ::MIME"text/plain", vpc::VPC)
   println(io, summary(vpc))
 end
 
-function quantile_t_data(sim,probabilities,dvname,times)
+function quantile_t_data(sim,probabilities,dvname,times,stratify_on::Nothing)
   df = DataFrame(t=times, dv=collect(Iterators.flatten([sim[i].observations[dvname] for i in 1:length(sim)])))
   df_quantile = by(df, :t, (low_quantile = :dv => t -> ( quantile(t, probabilities[1])), quantile = :dv => t -> ( quantile(t, probabilities[2])), high_quantile = :dv => t -> ( quantile(t, probabilities[3]))))
-  df_quantile
+  df_quantile, nothing
 end
 
-function quantile_t_sim(sim,probabilities,dvname,times)
+function quantile_t_data(sim,probabilities,dvname,times,stratify_on::Symbol)
+  df = DataFrame(t=times, dv=collect(Iterators.flatten([sim[i].observations[dvname] for i in 1:length(sim)])), cvs = collect(Iterators.flatten([[sim[i].covariates[stratify_on] for j in 1:length(sim[i].observations[dvname])] for i in 1:length(sim)])))
+  cvs__ = df[!,:cvs]
+  if length(unique(df[!,:cvs])) > 4
+      cvs_ = quantile(unique(df[!,:cvs]), [0.25,0.5,0.75,1.0])
+      cvs__ = map(x-> cvs_[sum([x>cvs_[1],x>cvs_[2],x>cvs_[3]])+1],df[!,:cvs])
+      df[!,:cvs] = cvs__
+  end
+  df_quantile = by(df, [:t,:cvs], (low_quantile = :dv => t -> ( quantile(t, probabilities[1])), quantile = :dv => t -> ( quantile(t, probabilities[2])), high_quantile = :dv => t -> ( quantile(t, probabilities[3]))))
+  groupby(df_quantile, :cvs), cvs__
+end
+
+function quantile_t_sim(sim,probabilities,dvname,times,cvs__::Nothing)
   df = DataFrame(t=times, dv=collect(Iterators.flatten([sim[i].observed[dvname] for i in 1:length(sim)])))
   df_quantile = by(df, :t, (low_quantile = :dv => t -> ( quantile(t, probabilities[1])), quantile = :dv => t -> ( quantile(t, probabilities[2])), high_quantile = :dv => t -> ( quantile(t, probabilities[3]))))
   df_quantile
 end
 
-function quantile_sub_sim(dfs,ci_probabilities,times)
+function quantile_t_sim(sim,probabilities,dvname,times,cvs__::AbstractVector)
+  df = DataFrame(t=times, dv=collect(Iterators.flatten([sim[i].observed[dvname] for i in 1:length(sim)])), cvs = cvs__)
+  df_quantile = by(df, [:t,:cvs], (low_quantile = :dv => t -> ( quantile(t, probabilities[1])), quantile = :dv => t -> ( quantile(t, probabilities[2])), high_quantile = :dv => t -> ( quantile(t, probabilities[3]))))
+  df_quantile
+end
+
+function quantile_sub_sim(dfs,ci_probabilities,times,stratify_on::Nothing)
   df_aggregate = DataFrame()
   map(df -> append!(df_aggregate, df), dfs)
-  df_sim_quantile =  by(df_aggregate, :t, (:low_quantile => t -> quantile(t, ci_probabilities), :quantile => t -> quantile(t, ci_probabilities),:high_quantile => t -> quantile(t, ci_probabilities)))
+  df_sim_quantile =  by(df_aggregate, [:t], (:low_quantile => t -> quantile(t, ci_probabilities), :quantile => t -> quantile(t, ci_probabilities),:high_quantile => t -> quantile(t, ci_probabilities)))
   return df_sim_quantile 
+end
+
+function quantile_sub_sim(dfs,ci_probabilities,times,stratify_on::Symbol)
+  df_aggregate = DataFrame()
+  map(df -> append!(df_aggregate, df), dfs)
+  df_sim_quantile =  by(df_aggregate, [:t,:cvs], (:low_quantile => t -> quantile(t, ci_probabilities), :quantile => t -> quantile(t, ci_probabilities),:high_quantile => t -> quantile(t, ci_probabilities)))
+  return groupby(df_sim_quantile, :cvs) 
 end
 
 function vpc(
@@ -36,14 +63,14 @@ function vpc(
   reps::Integer = 499;
   probabilities::NTuple{3,Float64} = (0.1, 0.5, 0.9),
   ci_level::Float64 = 0.95,
-  dvname::Symbol = :dv
+  dvname::Symbol = :dv,
+  stratify_on = nothing
   )
 
-  # FIXME! For now we assume homogenous sampling time across subjects. Eventually this should handle inhomogenuous sample times, e.g. with binning but preferably with some kind of Loess like estimator
   time = collect(Iterators.flatten([subject.time for subject in population]))
 
   # Compute the quantile of the samples
-  empirical = quantile_t_data(population,probabilities,dvname,time)
+  empirical, cvs__ = quantile_t_data(population, probabilities, dvname, time, stratify_on)
 
   # Simulate `reps` new populations
   sims = [simobs(m, population, param) for i in 1:reps]
@@ -51,10 +78,10 @@ function vpc(
   ci_probabilities = ((1 - ci_level)/2, (1 + ci_level)/2)
 
   # Compute the quantiles of the simulated data for the CIs
-  sim_quantile = map(sim -> quantile_t_sim(sim,probabilities,dvname,time), sims)
-  simulated = quantile_sub_sim(sim_quantile, ci_probabilities, time)
+  sim_quantile = map(sim -> quantile_t_sim(sim, probabilities, dvname, time, cvs__), sims)
+  simulated = quantile_sub_sim(sim_quantile, ci_probabilities, time, stratify_on)
 
-  return VPC(time, empirical, simulated, probabilities, ci_level)
+  return VPC(time, empirical, simulated, probabilities, ci_level, stratify_on, cvs__ != nothing ? unique(cvs__) : nothing)
 end
 
 """
@@ -71,26 +98,60 @@ vpc(fpm::FittedPumasModel, reps::Integer=499; kwargs...) = vpc(fpm.model, fpm.da
 
 @recipe function f(vpc::VPC)
   empirical_style = [:dashdot, :solid, :dot]
-  ribbon_simulated = hcat([vpc.simulated[!,i] for i in 2:4])
-  title --> "Confidence interval VPC"
-  for i in 1:3
-    @series begin
-      label --> "Empirical $(vpc.probabilities[i]*100)% quantile"
-      xlabel --> "time"
-      linewidth --> 2
-      linecolor --> :red
-      linestyle --> empirical_style[i]
-      vpc.empirical[!,1], vpc.empirical[!,i+1]
-    end
+  cvs = vpc.covariate_values
+  plot_title --> "Confidence interval VPC"
+  if :cvs in names(vpc.empirical)
+    layout --> length(vpc.empirical)
+    for cv_ind in 1:length(vpc.empirical)
+      legendfontsize --> 4
+      ribbon_simulated = hcat([vpc.simulated[cv_ind][!,i] for i in [:low_quantile_function, :quantile_function, :high_quantile_function]])
+      for i in 1:3
+        @series begin
+          subplot --> cv_ind
+          label --> "Empirical $(vpc.probabilities[i]*100)% quantile"
+          xlabel --> "time"
+          linewidth --> 2
+          linecolor --> :red
+          linestyle --> empirical_style[i]
+          legend --> cv_ind ==  length(vpc.empirical) ? :outerbottom : :false
+          vpc.empirical[cv_ind][!,1], vpc.empirical[cv_ind][!,i+2]
+        end
 
-    @series begin
-      label --> hcat(i == 3 ? "Simulated $(vpc.confidence_level*100)% confidence intervals" : "", "")
-      xlabel --> "time"
-      fillrange --> [[ribbon_simulated[i][j][1] for j in 1:length(ribbon_simulated[i])],[[ribbon_simulated[i][j][2] for j in 1:length(ribbon_simulated[i])]]]
-      fillcolor --> :blue
-      fillalpha --> 0.2
-      linewidth --> 0.0
-      vpc.simulated[!,1], hcat(vpc.empirical[!,i+1], vpc.empirical[!,i+1])
+        @series begin
+          subplot --> cv_ind
+          title --> "$(vpc.stratify_cvs)"*"<"*string(cvs[cv_ind])
+          label --> hcat(i == 3 ? "Simulated $(vpc.confidence_level*100)% confidence intervals" : "", "")
+          xlabel --> "time"
+          fillrange --> [[ribbon_simulated[i][j][1] for j in 1:length(ribbon_simulated[i])],[[ribbon_simulated[i][j][2] for j in 1:length(ribbon_simulated[i])]]]
+          fillcolor --> :blue
+          fillalpha --> 0.2
+          linewidth --> 0.0
+          legend --> cv_ind == length(vpc.empirical) ? :outerbottom : :false
+          vpc.simulated[cv_ind][!,1], hcat(vpc.empirical[cv_ind][!,i+2], vpc.empirical[cv_ind][!,i+2])
+        end
+      end
+    end
+  else
+    ribbon_simulated = hcat([vpc.simulated[!,i] for i in [:low_quantile_function, :quantile_function, :high_quantile_function]])
+    for i in 1:3
+      @series begin
+        label --> "Empirical $(vpc.probabilities[i]*100)% quantile"
+        xlabel --> "time"
+        linewidth --> 2
+        linecolor --> :red
+        linestyle --> empirical_style[i]
+        vpc.empirical[!,1], vpc.empirical[!,i+1]
+      end
+
+      @series begin
+        label --> hcat(i == 3 ? "Simulated $(vpc.confidence_level*100)% confidence intervals" : "", "")
+        xlabel --> "time"
+        fillrange --> [[ribbon_simulated[i][j][1] for j in 1:length(ribbon_simulated[i])],[[ribbon_simulated[i][j][2] for j in 1:length(ribbon_simulated[i])]]]
+        fillcolor --> :blue
+        fillalpha --> 0.2
+        linewidth --> 0.0
+        vpc.simulated[!,1], hcat(vpc.empirical[!,i+1], vpc.empirical[!,i+1])
+      end
     end
   end
 end
