79d18d46b30c4932619f941914ba07357d43b4c8
Vaibhavdixit02 <vaibhavyashdixit@gmail.com>
2020-01-25 13:54:51 +0530

    Generate subjects for hcv multiple dv test in the test file

--- test/nlme/information.jl
+++ test/nlme/information.jl
@@ -31,7 +31,13 @@ end
 @testset "Multiple dvs. (The HCV model)" begin
 
   hcv = PUMASMODELS["misc"]["HCV"]
-  peg_inf_model, _sub, param_PKPD = hcv["solver"], hcv["data"], hcv["param"]
+  peg_inf_model, param_PKPD = hcv["solver"], hcv["param"]
+
+  t = [0.0, 0.25, 0.5, 1.0, 2.0, 3.0, 4.0, 7.0, 10.0, 14.0, 21.0, 28.0]
+  
+  peg_inf_dr = DosageRegimen(180.0, ii=7.0, addl=3, duration=1.0)
+  _sub = Subject(id=1, evs=peg_inf_dr, time=t, obs=(yPK=zeros(length(t)), yPD=zeros(length(t))))
 
   @test logdet(Pumas._expected_information_fd(peg_inf_model, _sub, param_PKPD, zeros(7), Pumas.FO())*30) ≈ 92.21128100630904 rtol=1e-6
+
 end
